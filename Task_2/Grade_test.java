041ba294bedcbe59047b876a16b86fededdcdb00
Avanish More <avanishmore25@gmail.com>
2024-09-10 23:28:02 +0530

    Create Grade_test.java

---
+++ Task_2/Grade_test.java
@@ -0,0 +1,13 @@
+package com.codsoft;
+
+public class Grade_test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("\tStudent Grade Calculator");
+		Grade obj = new Grade();
+		obj.input();
+		
+	}
+	
+}
