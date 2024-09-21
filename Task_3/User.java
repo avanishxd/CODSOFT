069d9ab246b297e17409513880ef8543c2ae898f
Avanish More <avanishmore25@gmail.com>
2024-09-21 18:45:17 +0530

    Create User.java

---
+++ Task_3/User.java
@@ -0,0 +1,17 @@
+package com.jframe_swing;
+
+public class User {
+	
+	//Additional i will add user info such as account number, branch, etc. It will require different windows which has to be designed with swing
+	
+	int Bal = 1000;
+	
+	public int getBal() {
+		return Bal;
+	}
+	
+	public void setBal(int Bal) {
+		this.Bal = Bal;
+	}
+	
+}
